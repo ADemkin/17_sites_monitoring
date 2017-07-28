85c410cbd723768411015e5aad670f96de579407
antondemkin <antondemkin@yandex.ru>
2017-07-28 06:48:35 +0300

    First commit.

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+This script will help you monitor sites health
+It will check web site status and domain expiration time left.
+# Installation:
+Python 3 required.
+
+To install additional packages you should run this command from folder with where you downloaded script.
+```
+pip3 instal -r requirements.txt
+```
+
+# Usage:
+
+You need to create a text file with urls you want to check. 
+
+sites.txt:
+```
+facebook.com
+ya.ru
+megaupload.com
+specialized.com
+```
+
+Run this script using python 3 with a path as an argument.
+```
+check_sites_health.py [path]
+```
+
+Example:
+```
+check_sites_health.py sites.txt
+...
+facebook.com status: ok
+Domain health ok. 2801 days until expire.
+...
+ya.ru status: ok
+Warning! Domain will expire in 3 days
+...
+megaupload.com is down!
+Domain health ok. 236 days until expire.
+...
+specialized.com status: ok
+Domain health ok. 3384 days until expire.
+
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,14 +1,6 @@
-import urllib.request
-import os
 import sys
-import ssl
-import urllib.parse
-import requests
-
 import requests
 import datetime
-import json
-
 import re
 
 
@@ -16,68 +8,53 @@ def load_urls4check(path):
     with open(path) as file:
         # remove http:// or https:// and www. from url
         urls = re.sub('(https?)?:\/\/(www\.)?', '', file.read()).split('\n')
-    
     return urls
-
-
-def parse_urls4http(urls):
-    pass
 
 
 def is_server_respond_with_200(url):
     try:
         request = requests.get(url='http://' + url)
-    except requests.exceptions.ConnectionError as error:
-        print(error)
+    except requests.exceptions.ConnectionError:
+        return False
     else:
-        ok_code = 200
-        if request.status_code is ok_code:
+        ok = 200
+        if request.status_code is ok:
             return True
 
 
-def get_domain_expiration_date(url):
+def get_domain_time_untill_expire(url):
+    # using api for whoapi.com to check whois on a certain domain
     params = {"apikey":'a7752dbdc9f6086b833baaca5a3f00a9', 'r':'whois', 'domain':'http://' + url}
     request = requests.get('http://api.whoapi.com/', params=params)
-    data = request.json()["date_expires"]
-    # correct_data = re.sub("[A-Z]", ' ', data)
-    correct_data = data
-    print(correct_data)
-    return correct_data
-
-
-def get_time_untill_expire(date):
-    now = datetime.datetime.now()
-    
-    # sometimes we see non-standart date format like 2019-03-09-T19:06:45Z, so we need to catch an error
-    try:
-        expiration_date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    except ValueError as error:
-        print(error)
-        # return 'Unknown'
-    else:
+    answer = request.json()
+    if answer["registered"]:
+        expiration_date = datetime.datetime.strptime(answer["date_expires"], '%Y-%m-%d %H:%M:%S')
+        now = datetime.datetime.now()
         time_untill_expire = expiration_date - now
         return time_untill_expire.days
 
 
-def print_domain_health(url, ok_status, days_left):
-    print("%s status: " % url, end='')
-    if ok_status:
-        print("ok", end=' ')
-    else:
-        print("NOT RESPONDING", end=' ')
-    if days_left < 30:
-        print("WARNING, DOMAIN IS EXPIRING SOON: ", end='')
-    else:
-        pass
-    print("%d days left" % days_left)
-
-
 if __name__ == '__main__':
-    path = sys.argv[1]
-    # add check for argv len
-    urls_to_check = load_urls4check(path)
-    print(urls_to_check)
-    for url in urls_to_check:
-        ok_status = is_server_respond_with_200(url)
-        days_till_expire = get_time_untill_expire(get_domain_expiration_date(url))
-        print_domain_health(url, ok_status, days_till_expire)
+    if len(sys.argv) > 1:
+        
+        path = sys.argv[1]
+        urls_to_check = load_urls4check(path)
+        
+        for url in urls_to_check:
+            print('...')
+            
+            if is_server_respond_with_200(url):
+                print("%s status: ok" % url)
+            else:
+                print("%s is down!" % url)
+            
+            expiration_info = get_domain_time_untill_expire(url)
+            if expiration_info:
+                if expiration_info > 30:
+                    print('Domain health ok. %d days until expire.' % expiration_info)
+                else:
+                    print("Warning! Domain will expire in %s days" % expiration_info)
+            else:
+                print("Domain is not registered.")
+    else:
+        print("Usage: check_sites_health.py [path]")
